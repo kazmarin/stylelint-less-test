fcb23d943515c6162a14b5b1674c11cca06ba770
Nikolay Gromov <nicothin@gmail.com>
2016-03-04 22:19:54 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const gulp = require('gulp');
+const reporter = require("postcss-reporter");
+const postcssLess = require("postcss-less");
+const stylelint = require("stylelint");
+
+gulp.task("lint:less", function () {
+  return gulp.src("src/less/lint-test.less")
+    .pipe(postcss([
+      stylelint({
+        "rules": {
+          "color-no-invalid-hex": true,
+          "max-empty-lines": 2,
+          "indentation": ["tab", {
+            "except": ["value"]
+          }]
+        }
+      }),
+      reporter({ clearMessages: true }),
+    ], {
+      syntax: postcssLess.default
+    }));
+});
