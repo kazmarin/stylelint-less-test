fdaa179294ce3715538dc1680638905b1eb829e1
Nikolay Gromov <nicothin@gmail.com>
2016-06-23 17:40:47 +0300

    Включённый, но пока не полностью настроенный линтинг

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,12 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const reporter = require("postcss-reporter");
-const postcssLess = require("postcss-less");
 const stylelint = require("stylelint");
 
-gulp.task("lint:less", function () {
-  return gulp.src("style.less")
+gulp.task('lint:less', function () {
+  return gulp.src('style.less')
     .pipe(postcss([
-      stylelint({
-        "rules": {
-          "color-no-invalid-hex": true,
-          "max-empty-lines": 2,
-          "indentation": ["tab", {
-            "except": ["value"]
-          }]
-        }
-      }),
-      reporter({ clearMessages: true }),
-    ], {
-      syntax: postcssLess.default
-    }));
+        stylelint(),
+        reporter({clearMessages: true})
+    ]));
 });
